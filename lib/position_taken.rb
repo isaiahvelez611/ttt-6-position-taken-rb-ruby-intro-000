2ac9d6882129382585662fa7884ce6c3aad641f1
Isaiah Velez <isaiah.velez611@gmail.com>
2019-12-03 01:40:23 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,7 +3,7 @@
  def position_taken?(board, index)
   if board[index] == " " || ""
     return false
-  elsif board[index] == "X" || "O"
+  elsif board[index] == "X" or "O"
     return true
   end
 end
